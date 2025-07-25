770d27186417cb4b888252542b673905bd51664d
Mena <dayjacs03@gmail.com>
2025-07-25 18:00:38 -0500

    wip

--- rockpaper.js
+++ rockpaper.js
@@ -2,14 +2,39 @@
 let counter = 5
 let choices = ["rock", "paper", "scissors"]
 
-let usr_input = console.log(prompt("rock, paper, or scissors?: "));
+function game() {
+    playround();
+}
+
+function playRound() {
+    let usrchoice = playerChoice();
+    let computerSelection = computerChoice();
+}
 
 function playerChoice() {
-    usr_input;
+    let input = prompt("rock, paper, or scissors?: ");
+
+    while(input == null) {
+        input = prompt("rock, paper, or scissors?: ");
+    }
+
+    input = input.toLowerCase;
+    let check = validateInput(input)
+    if( check == true) {
+        console.log(input)
+    }
 }
+
+function validateInput(choice) {
+        if(choices.includes(choice)) {
+        return true;
+    } else {
+           return false;
+        }
+    }
 
 function computerChoice() {
-    
+    return choices[Math.floor(Math.random()*choices.length)]
 }
 
-
+game();

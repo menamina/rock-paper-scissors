ac5a4c4b04e784eee324434a5e1a8405c5232f19
Mena <dayjacs03@gmail.com>
2025-07-25 17:31:46 -0500

    adding

---
+++ rockpaper.js
@@ -0,0 +1,15 @@
+// 5 rounds; play on console
+let counter = 5
+let choices = ["rock", "paper", "scissors"]
+
+let usr_input = console.log(prompt("rock, paper, or scissors?: "));
+
+function playerChoice() {
+    usr_input;
+}
+
+function computerChoice() {
+    
+}
+
+
